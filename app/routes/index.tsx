f02b8f9e8b152bdb13c1260804926cdbef758349
martindevaluado <pmartinacosta@gmail.com>
2022-03-06 17:20:41 -0300

    change styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
 export default function Index() {
   return (
-    <div className="flex flex-col md:flex-row w-full pt-6 md:pl-6 gap-16 lg:w-3/4">
+    <div className="flex flex-col md:flex-row w-full pt-6 md:pl-6 gap-8 md:gap-14 lg:w-3/4">
       <div className="md:w-1/2">
         <h3 className="mb-4 leading-tight text-2xl md:text-4xl font-semibold">
           FrontEnd Developer React
