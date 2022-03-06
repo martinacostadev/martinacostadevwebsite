122af883b1438fb6a0787d3cf448ba52f868e4b9
martindevaluado <pmartinacosta@gmail.com>
2022-03-06 17:05:31 -0300

    remove: Blog section

--- app/component/Menu/index.tsx
+++ app/component/Menu/index.tsx
@@ -4,7 +4,7 @@ export function Menu() {
     return (
         <div className="flex flex-row space-x-8 md:text-lg mt-12 md:mt-0">
             <Link to="/" className="hover:underline hover:underline-offset-4 text-gray-300 font-bold hover:text-white">Inicio</Link>
-            <Link to="/blog" className="hover:underline hover:underline-offset-4 text-gray-300 font-bold hover:text-white">Blog</Link>
+            {/* <Link to="/blog" className="hover:underline hover:underline-offset-4 text-gray-300 font-bold hover:text-white">Blog</Link> */}
             <Link to="/advices" className="hover:underline hover:underline-offset-4 text-gray-300 font-bold hover:text-white">Consejos</Link>
             <Link to="/aboutme" className="hover:underline hover:underline-offset-4 text-gray-300 font-bold hover:text-white">Acerca de</Link>
         </div>
